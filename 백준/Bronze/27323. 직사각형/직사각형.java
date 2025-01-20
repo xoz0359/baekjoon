98f6ee0d9bbdd32a5f5f43778726b35976419fc6
xoz0359 <k84890690@gmail.com>
2025-01-20 16:02:24 +0900

    [Bronze V] Title: 직사각형, Time: 176 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 사각형의 넓이를 구하는 프로그램
+		Scanner input = new Scanner(System.in);
+		int a = input.nextInt();
+		int b = input.nextInt();
+		
+		System.out.println(a*b);
+	}
+}
