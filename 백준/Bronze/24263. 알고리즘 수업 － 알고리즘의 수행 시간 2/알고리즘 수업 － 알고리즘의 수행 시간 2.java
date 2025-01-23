447143ded56fcaa4de8f0f2974b3151cf2678402
xoz0359 <k84890690@gmail.com>
2025-01-23 21:33:49 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 176 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 백준의 시간복잡도 2번 문제
+		// 함수 안에 parameter n을 제어변수로 사용하는 반복문이 하나 있음으로
+		// 이 코드의 실행 횟수는 pow(n, 1)번이고 최대 차항은 1이다
+		Scanner input = new Scanner(System.in);
+		int a = input.nextInt();
+		System.out.println(a);
+		System.out.println(1);	
+	}
+}
