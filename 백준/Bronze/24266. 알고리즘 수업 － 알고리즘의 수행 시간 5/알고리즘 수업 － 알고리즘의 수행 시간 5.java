16858e6f0295d4ba1487b411a76fdedbe3882be9
xoz0359 <k84890690@gmail.com>
2025-01-24 17:36:02 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 172 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,16 @@
+	import java.util.*;
+	
+	public class Main {
+	
+		public static void main(String[] args) {
+			// 백준의 시간복잡도 5번 문제
+			
+			Scanner input = new Scanner(System.in);
+			long a = input.nextInt(); 
+			// 반복문이 3개니까 시간 복잡도는 Opow(n, 3)
+			long sum = 0;
+			System.out.println(a*a*a);
+			System.out.println(3);
+			
+		}
+	}
