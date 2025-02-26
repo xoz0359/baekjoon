6b823c3bdb47c5ead5c528673b736a2aa3136445
xoz0359 <k84890690@gmail.com>
2025-02-26 17:47:18 +0900

    문제 풀이 중에 while문 하나로 중첩반복문을 구현한 게 재미있어서 저장합니다 물론 시간 복잡도 관점에서 정답과는 거리가 먼 코드 입니다

--- baekjoon/src/baekjoon/Main.java
+++ baekjoon/src/baekjoon/Main.java
@@ -5,18 +5,36 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	
 
-	public static void main(String[] args) {
-		
-		//BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+
+		Main mi = new Main();
+		// BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		Scanner input = new Scanner(System.in);
-		Stack <Integer> s = new Stack<Integer>();
-		StringBuffer sb = new StringBuffer(); 
-		String str = input.nextLine();
-		int strleng = str.length();
-		while(input.hasNextLine()) {
-			
+		int a = input.nextInt();
+		int b = input.nextInt();
+		StringBuffer sb = new StringBuffer();
+		sb.append("<");
+		int cnt = 0, cnt2 = a , num = 0, cnt3 = 0;
+		while(cnt2 > 0) {
+			System.out.println("cnt:"+cnt+"cnt2:"+cnt2+"num:"+num);
+			if (sb.indexOf(num+"")!=-1) {
+				
+				num = num >= a ? 1: num+1;
+				//System.out.println(sb.toString());
+				continue;
+			}
+			if (cnt == b) {
+				sb.append(num+" ");
+				cnt2--;
+				cnt = 0;
+				//System.out.println(cnt2);
+			}
+			num = num >= a ? 1: num+1;
+			cnt++;
 		}
+		String str = sb.substring(0, sb.length()-1);
+		str = str.replace(" ", ", ");
+		System.out.println(str+">");
 	}
 }
