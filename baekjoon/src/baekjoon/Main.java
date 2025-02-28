be53e23a4597be06ddc2ac8873c35383a3cb7fc5
xoz0359 <k84890690@gmail.com>
2025-02-28 21:21:19 +0900

    문제풀이 저장용 커밋입니다

--- baekjoon/src/baekjoon/Main.java
+++ baekjoon/src/baekjoon/Main.java
@@ -2,43 +2,80 @@ package baekjoon;
 
 import java.io.*;
 import java.util.*;
-import java.io.*;
 
 public class Main {
+	int size;
+	int arr[];
+	
+	public Main() {
+		arr = new int[0];
+		size = 0;
+	}
+	
+	public void push_front(int num) {
+		size++;
+		arr = new int[size];
+	}
+	
+	public void push_back(int num) {
+		
+	}
+	
+	public int pop_front(int num) {
+		
+		return -1;
+	}
+	
+	public int pop_back(int num) {
+		
+	}
+	
+	public int size() {
+		
+		return size;
+	}
+	
+	public int empty() {
+		
+	}
+	
+	public int front() {
+		
+	}
+	
+	public int back() {
+		
+	}
 
 	public static void main(String[] args) throws IOException {
 
 		Main mi = new Main();
-		// BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-		Scanner input = new Scanner(System.in);
-		int a = input.nextInt();
-		int b = input.nextInt();
-		StringBuffer sb = new StringBuffer();
-		// 시간 복잡도를 줄여보자
-		int list[] = new int[a];
-		for (int i = 1; i <= a; i++) {
-			list[i-1] = i;
-		}
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		//Scanner input = new Scanner(System.in);
+		int a = Integer.parseInt(input.readLine());
 		
-		
-		sb.append("<");
-		int cnt = 0, cnt2 = 0;
-		while (cnt2 < a) {
-			for (int i = 0; i < a; i++) {
-				if (list[i]==0) continue;
-					cnt++;
-				if (cnt == b) {
-					cnt2++;
-					cnt = 0;
-					sb.append(list[i] + " ");
-					list[i] = 0;
-				}
-	
+		for (int i = 0; i < a; i++) {
+			String str = input.readLine();
+			if (str.equals("")) {
+				
+			}else if (str.equals("size")) {
+				
+			}else if (str.equals("empty")) {
+				
+			}else if (str.equals("front")) {
+				
+			}else if (str.equals("back")) {
+				
+			}else if (str.equals("push_front")) {
+				
+			}else if (str.equals("push_back")) {
+				
+			}else if (str.substring(0, 4).equals("pop_f")) {
+				
+			}else  {
+				
 			}
-			//System.out.println(sb.toString());
 		}
-		String str = sb.substring(0, sb.length() - 1);
-		str = str.replace(" ", ", ");
-		System.out.println(str + ">");
+		
 	}
 }
