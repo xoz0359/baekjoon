448bc45e5e0757f3ce13b71e79824c37a49909b5
xoz0359 <k84890690@gmail.com>
2025-02-28 21:21:00 +0900

    문제풀이 저장용 커밋입니다

--- baekjoon/src/baekjoon/Main.java
+++ baekjoon/src/baekjoon/Main.java
@@ -15,37 +15,27 @@ public class Main {
 		int b = input.nextInt();
 		StringBuffer sb = new StringBuffer();
 		// 시간 복잡도를 줄여보자
-		int arr[] = new int[a];
+		int list[] = new int[a];
+		for (int i = 1; i <= a; i++) {
+			list[i-1] = i;
+		}
+		
+		
 		sb.append("<");
-		int cnt = 0, num = 0, cnt2 = 0;
-		boolean c = true;
-		while (cnt2 <= a) {
-			cnt++;
-			num++;
-			num = num > a ? 1 : num;
-			System.out.println(num+" "+cnt);
-			c = true;
-			if (cnt == b) {
-				cnt = 0;
-
-				// 여기서 출구가 하나인 자료구조를 사용하면 시간복잡도를 더 줄일 수 있을 것 같다
-				for (int i = num+cnt2; i < arr.length; i++) {
-					if (arr[i - 1] == 0) {
-						arr[num - 1] = 1;
-						sb.append(i + " ");
-						break;
-					}
-					for (int j = 1; j <= num+cnt2; j++) {
-						if (arr[j - 1] == 0) {
-							arr[num - 1] = 1;
-							sb.append(j + " ");
-							break;
-						}
-					}
-					
+		int cnt = 0, cnt2 = 0;
+		while (cnt2 < a) {
+			for (int i = 0; i < a; i++) {
+				if (list[i]==0) continue;
+					cnt++;
+				if (cnt == b) {
+					cnt2++;
+					cnt = 0;
+					sb.append(list[i] + " ");
+					list[i] = 0;
 				}
-				cnt2++;
+	
 			}
+			//System.out.println(sb.toString());
 		}
 		String str = sb.substring(0, sb.length() - 1);
 		str = str.replace(" ", ", ");
